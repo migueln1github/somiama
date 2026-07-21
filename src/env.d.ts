d857cb732a04cb04e40340b46d5ac026fa884998
migueln1github <168670555+migueln1github@users.noreply.github.com>
2026-07-21 22:40:58 +0200

    Delete src directory

--- src/env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="../.astro/types.d.ts" />
-
-interface ImportMetaEnv {
-  readonly PUBLIC_CLERK_PUBLISHABLE_KEY: string;
-}
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
